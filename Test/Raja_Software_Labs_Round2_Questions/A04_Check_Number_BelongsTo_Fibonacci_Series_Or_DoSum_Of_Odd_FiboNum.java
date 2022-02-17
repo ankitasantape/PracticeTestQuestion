9df1c58d7e28d94605dadbfb83c9fb470514f62b
Ankita Santape <54230924+ankitasantape@users.noreply.github.com>
2022-02-17 13:24:07 +0530

    Update A04_Check_Number_BelongsTo_Fibonacci_Series_Or_DoSum_Of_Odd_FiboNum.java

--- Test/Raja_Software_Labs_Round2_Questions/A04_Check_Number_BelongsTo_Fibonacci_Series_Or_DoSum_Of_Odd_FiboNum.java
+++ Test/Raja_Software_Labs_Round2_Questions/A04_Check_Number_BelongsTo_Fibonacci_Series_Or_DoSum_Of_Odd_FiboNum.java
@@ -13,6 +13,35 @@ public class A04_Check_Number_BelongsTo_Fibonacci_Series_Or_DoSum_Of_Odd_FiboNum
 		System.out.println(getFiboCount(21)); // 21 is a fibonacci number
 		System.out.println(getFiboCount(23)); // 44
 		
+		  int n = 7;
+		  int a = 0;
+		  int b = 1;
+		  int c = a + b;
+		  int sum = 1;
+		  while( c < n ) {
+			  if(c % 2 != 0) {
+				  sum += c;
+			  }
+			  a = b;
+			  b = c;
+			  c = a + b;
+		  }
+		  System.out.println(sum); // output: 10
+		
+		  int n1 = 7;
+		  int a1 = 0;
+		  int b1 = 1;
+		  int c1 = a1 + b1;
+		  int sum1 = 0;
+		  while( b1 < n ) {
+			  if(b1 % 2 != 0) {
+				  sum1 += b1;
+			  }
+			  a1 = b1;
+			  b1 = c1;
+			  c1 = a1 + b1;
+		  }
+		  System.out.println(sum1); // output: 10
 	}
 	
 	public static int getFiboCount(int n) {
