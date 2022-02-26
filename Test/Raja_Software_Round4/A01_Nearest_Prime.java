4fd59630a37c95d2ac2d79d6c7b28e98c47c92a6
ankitasantape <ankitasantape99@gmail.com>
2022-02-26 12:51:58 +0530

    Added files

--- Test/Raja_Software_Round4/A01_Nearest_Prime.java
+++ Test/Raja_Software_Round4/A01_Nearest_Prime.java
@@ -6,7 +6,45 @@ public class A01_Nearest_Prime {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int leftdist = 0;
+		int rightdist = 0;
+		int smallerNearestPrime = 0;
+		int greaterNearestPrime = 0;
+		int i = n;
+		while( i++ > 0 ) {
+			if( isPrime( i ) ) {
+				rightdist = i - n;
+				greaterNearestPrime = i;
+				break;
+			}
+		}
+		i = n;
+		while( i-- > 0 ) {
+			if( isPrime( i ) ) {
+				leftdist = n - i;
+				smallerNearestPrime = i;
+				break;
+			}
+		}
+		if( leftdist < rightdist ) {
+			System.out.println("Closest prime number is " + smallerNearestPrime);
+		} else if( rightdist < leftdist ) {
+			System.out.println("Closest prime number is " + greaterNearestPrime);
+		} else {
+			System.out.println( "Closest prime numbers are " + smallerNearestPrime + " and " + greaterNearestPrime);
+		}
 		
+		sc.close();
+	}
+	
+	public static boolean isPrime(int n) {
+		for(int div = 2; div * div <= n; div++) {
+			if( n % div == 0 ) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
